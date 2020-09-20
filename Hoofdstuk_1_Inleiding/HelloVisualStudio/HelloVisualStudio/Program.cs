ea2c04c73a492f5b9d7f24c17c9ca8ff7af48880
DeSmetElias <elias.desmet@student.hogent.be>
2020-09-20 15:28:36 +0200

    Add HelloVisualStudio

---
+++ Hoofdstuk_1_Inleiding/HelloVisualStudio/HelloVisualStudio/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*
+ * Shortcuts
+ *
+ * Ctrl + k + s --> code snippet
+ *
+ *
+*/
+
+namespace HelloVisualStudio {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(SayHello());
+        }
+
+        // Method SayHello
+        private static string SayHello() {
+            return "Hello C#";
+        }
+    }
+}
+
+
+
+
+
+
