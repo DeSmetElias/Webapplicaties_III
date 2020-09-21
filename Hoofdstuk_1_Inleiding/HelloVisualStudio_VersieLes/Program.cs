eb5ff8917820153557c42588bda6397eaa022c3a
DeSmetElias <elias.desmet@student.hogent.be>
2020-09-21 14:15:32 +0200

    Add SayHello

--- Hoofdstuk_1_Inleiding/HelloVisualStudio_VersieLes/Program.cs
+++ Hoofdstuk_1_Inleiding/HelloVisualStudio_VersieLes/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Net;
 
 namespace HelloVisualStudio_VersieLes {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(SayHello());
+        }
+
+        private static string SayHello() {
+            return "Hello C#";
         }
     }
 }
