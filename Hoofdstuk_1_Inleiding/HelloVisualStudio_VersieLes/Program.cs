e957f567326c8b5e138cce6cd2edf3cad27e875e
DeSmetElias <elias.desmet@student.hogent.be>
2020-09-21 14:00:01 +0200

    HelloVisualStudio_VersieLes -- Les 1: ma 21/09/20

---
+++ Hoofdstuk_1_Inleiding/HelloVisualStudio_VersieLes/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace HelloVisualStudio_VersieLes {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
