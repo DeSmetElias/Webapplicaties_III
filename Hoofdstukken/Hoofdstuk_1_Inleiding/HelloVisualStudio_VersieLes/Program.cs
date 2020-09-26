404444292e3efb6f57b3465c99593f5f537693c1
DeSmetElias <elias.desmet@student.hogent.be>
2020-09-26 14:46:09 +0200

    changes dir structure

---
+++ Hoofdstukken/Hoofdstuk_1_Inleiding/HelloVisualStudio_VersieLes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+
+namespace HelloVisualStudio_VersieLes {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(SayHello());
+        }
+
+        private static string SayHello() {
+            return "Hello C#";
+        }
+    }
+}
