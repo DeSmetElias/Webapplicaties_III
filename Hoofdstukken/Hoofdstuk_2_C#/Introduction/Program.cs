ad7d5d7e57ba7ca923b5019fb880ad490f5c5f55
DeSmetElias <elias.desmet@student.hogent.be>
2020-09-26 15:24:15 +0200

    Add 4 outputs

--- Hoofdstukken/Hoofdstuk_2_C#/Introduction/Program.cs
+++ Hoofdstukken/Hoofdstuk_2_C#/Introduction/Program.cs
@@ -1,9 +1,47 @@
 ﻿using System;
 
 namespace Introduction {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+    class Program { // class
+        static void Main(string[] args) { // main method
+            Output4();
+        }
+
+        private static void Output1() {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+
+        private static void Output2() {
+            Console.WriteLine("    /|");
+            Console.WriteLine("   / |");
+            Console.WriteLine("  /  |");
+            Console.WriteLine(" /___|");
+
+            Console.ReadLine();
+        }
+
+        private static void Output3() {
+            string charachterName = "George";
+            int charachterAge = 70;
+
+            Console.WriteLine("There once was a man named " + charachterName);
+            Console.WriteLine("He was " + charachterAge + " years old");
+            Console.WriteLine("He really liked the name " + charachterName);
+            Console.WriteLine("But didn't like being " + charachterAge);
+
+            Console.ReadLine();
+        }
+
+        private static void Output4() {
+            string phrase = "Giraffe Acadamy";
+            char grade = 'A';
+            int age = 30;
+            double gpa = 3.2;
+            bool isMale = false;
+
+            Console.WriteLine(phrase);
+
+            Console.ReadLine();
         }
     }
 }
