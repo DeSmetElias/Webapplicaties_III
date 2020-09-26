59e5ba79e6f8bd8e21d4e3057d9d9b6e63a8e7de
DeSmetElias <elias.desmet@student.hogent.be>
2020-09-26 16:44:40 +0200

    2u24m54s in video C# Tutorial - Full Course for Beginners

--- Hoofdstukken/Hoofdstuk_2_C#/Introduction/Program.cs
+++ Hoofdstukken/Hoofdstuk_2_C#/Introduction/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using System.Transactions;
 
 namespace Introduction {
     class Program { // class
@@ -70,8 +71,101 @@ namespace Introduction {
             Console.ReadLine(); // shows the output
         }
 
+        private static void Output7() {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+
+            Console.ReadLine();
+        }
+
+        private static void Calculator1() {
+            Console.WriteLine("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = num1 + num2;
+
+            Console.WriteLine(sum);
+
+            Console.ReadLine();
+        }
+
+        private static void Output8() {
+
+            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+            string[] friends = new string[5];
+            friends[0] = "Elias";
+            // { "Elias", "Inge", "Joell", "Geert" };
+
+            Console.WriteLine(luckyNumbers[2] + " == 15");
+            luckyNumbers[0] = 100;
+            Console.WriteLine(luckyNumbers[0] + " == 100");
+            Console.WriteLine(friends[0]);
+
+            Console.ReadLine();
+        }
+
+        private static void Output9() { // if and else
+            bool isMale = false;
+            bool isFemale = false;
+
+            if (isMale) {
+                Console.WriteLine("You are male");
+            } else {
+                if (isFemale) {
+                    Console.WriteLine("You are a female");
+                } else {
+                    Console.WriteLine("You are X");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Output10() {  // switch statement
+            Console.WriteLine("Give a number (0 -> Monday, 6 -> Sunday)");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            String day;
+
+            switch (number) {
+                case 0:
+                    day = "Monday";
+                    break;
+                case 1:
+                    day = "Tuesday";
+                    break;
+                case 2:
+                    day = "Wednesday";
+                    break;
+                case 3:
+                    day = "Thursday";
+                    break;
+                case 4:
+                    day = "Friday";
+                    break;
+                case 5:
+                    day = "Saturday";
+                    break;
+                case 6:
+                    day = "Sunday";
+                    break;
+                default:
+                    day = String.Format("Not valid number (" + number + ")");
+                    break;
+
+            }
+
+            Console.WriteLine(day);
+
+            Console.ReadLine();
+        }
+
         static void Main(string[] args) { // main method
-            Output6();
+            Output10();
         }
     }
 }
